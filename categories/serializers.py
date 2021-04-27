d38584d8b0ef96089b2de6b4c64fc722cfa8ec07
m_martsul <martsul.mary@gmil.com>
2021-04-28 01:20:52 +0300

    Commit First

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,6 +5,6 @@ from categories.models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
         model = Category
 
-        fields = "__all__"
