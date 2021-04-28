07b95a6ff2203b964dcb4b9ba0aabf6911d6377d
m_martsul <martsul.mary@gmil.com>
2021-04-28 03:06:51 +0300

    Commit First

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-
 from categories.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
+        fields = ('id', 'objects', 'name', 'slug', 'created_at', 'updated_at',)
         model = Category
-
